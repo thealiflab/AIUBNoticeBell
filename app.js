6046dbaf691f4dc2b70555e9b76450767da1e6ce
Alif <mail@ahmedalif.com>
2020-06-01 17:34:38 +0600

    Works fine without proper form validation

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const {check, validationResult} = require('express-validator');
+// const session = require('express-session');
+// const hbs = require('express-hbs');
 var mysql = require("mysql");
+
 
 
 const app = express();
 
-app.use(express.static("public"));  //To use local assets
+app.use(express.static("Front-End"));  //To use local assets
 app.use(bodyParser.urlencoded({extended: true}));
 
+// app.engine('hbs', hbs.express4({
+//     partialsDir: __dirname + '/views/partials'
+// }));
+// app.set('view engine', 'hbs');
+// app.set('views', __dirname + '/views/partials');
+
+
+
 app.get("/",function(req,res){
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/Front-End/index.html");
 });
 
-app.listen(3306, function(){
+app.listen(3000, function(){
     console.log("Server started....");
 });
 
@@ -36,27 +48,60 @@ connection.connect(function(err){
 
 
 
-app.post("/",function(req,res){
-    var aiubname = req.body.name;
-    var username = req.body.username;
-    var mail = req.body.email;
-    var phoneno = req.body.phone;
+app.post("/", [
+    check('name').not().isEmpty().escape().withMessage('Name must have more than 6 characters'),
+    check('username', 'Username is empty').not().isEmpty(),
+    check('email', 'Your email is not valid').not().isEmpty().isEmail().normalizeEmail(),
+    check('phone', 'Your phone number is invalid').not().isNumeric(),
+
+],function(req,res){
+
+    // var errors = validationResult(req).array();
+    // if (errors) {
+    //     req.session.errors = errors;
+    //     req.session.success = false;
+    //     res.redirect("/");
+    // } else {
+    //     req.session.success = true;
+    //     res.redirect("/");
+    // }
+
+    var aiubname = req.body.name.trim().toUpperCase()
+    var username = req.body.username.trim();
+    var mail = req.body.email.trim();
+    var phoneno = req.body.phone.trim();
     var gender = req.body.gender;
 
-    console.log(firstName,lastName,username,email,phone,gender);
+    console.log(aiubname,username,mail,phoneno,gender);
 
+    connection.query('SELECT username FROM registration WHERE username ="' +username+'"', function (err, result) {
+        if (err) throw err;
 
-    var sql = "insert into usersinfo values('"+firstName+"', '"+lastName+"', '"+username+"','"+email+"', '"+phone+"', '"+gender+"')"
-    connection.query(sql, function (err) {
-        if (err){
-            res.sendFile(__dirname+"/fail.html");
-            console.log(err);
-            return;
+        console.log(result.length);
+        //checking duplicate username
+        if(result.length == 0){
+            var sql = "insert into registration values('','"+aiubname+"', '"+username+"', '"+mail+"','"+phoneno+"', '"+gender+"')";
+            
+            connection.query(sql, function (err) {
+                if (err){
+                    res.sendFile(__dirname+"/Front-End/fail.html");
+                    console.log(err);
+                    return;
+                }
+                else{
+                    res.sendFile(__dirname+"/Front-End/subscribed.html");
+                }
+            });
+            console.log("Username not exist, easy to enter data into database");
+        }else{
+            console.log('Username exist!!');
+            res.sendFile(__dirname+"/Front-End/fail.html");
         }
-        else{
-            res.sendFile(__dirname+"/success.html");
-        }
+    
+
       });
+
+    
 
     //connection.end();
 
