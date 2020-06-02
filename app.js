aa4086f57fc1fec4e8f191b4075f0e3a44dae0c9
Alif <mail@ahmedalif.com>
2020-06-02 09:14:12 +0600

    Back-end validation initially done, added twig

--- app.js
+++ app.js
@@ -2,27 +2,20 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const {check, validationResult} = require('express-validator');
-// const session = require('express-session');
-// const hbs = require('express-hbs');
 var mysql = require("mysql");
-
-
 
 const app = express();
 
 app.use(express.static("Front-End"));  //To use local assets
 app.use(bodyParser.urlencoded({extended: true}));
-
-// app.engine('hbs', hbs.express4({
-//     partialsDir: __dirname + '/views/partials'
-// }));
-// app.set('view engine', 'hbs');
-// app.set('views', __dirname + '/views/partials');
+app.set('view engine', 'twig');
+app.set('views','./Front-End/views');
 
 
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname + "/Front-End/index.html");
+    //res.sendFile(__dirname + "/Front-End/index.html");
+    res.render('index');
 });
 
 app.listen(3000, function(){
@@ -47,14 +40,23 @@ connection.connect(function(err){
 });
 
 
-
 app.post("/", [
-    check('name').not().isEmpty().escape().withMessage('Name must have more than 6 characters'),
-    check('username', 'Username is empty').not().isEmpty(),
-    check('email', 'Your email is not valid').not().isEmpty().isEmail().normalizeEmail(),
-    check('phone', 'Your phone number is invalid').not().isNumeric(),
+    check('name','Invalid Name').not().isEmpty().escape().isLength({min: 6}),
+    check('username', 'Invalid Username').not().isEmpty().escape().isLength({min: 4}),
+    check('email', 'Invalid Email').not().isEmpty().isEmail().escape().normalizeEmail(),
+    check('phone', 'Invalid Phone Number').not().isEmpty().escape().isNumeric().isLength({min: 11}),
 
 ],function(req,res){
+
+    const errors = validationResult(req);
+    console.log(errors.mapped());
+    res.render('index',{
+        error: errors.mapped()
+    });
+
+    // if (!errors.isEmpty()) {
+    //     return res.status(422).json({ errors: errors.array() });
+    // }
 
     // var errors = validationResult(req).array();
     // if (errors) {
@@ -66,40 +68,42 @@ app.post("/", [
     //     res.redirect("/");
     // }
 
-    var aiubname = req.body.name.trim().toUpperCase()
-    var username = req.body.username.trim();
-    var mail = req.body.email.trim();
-    var phoneno = req.body.phone.trim();
-    var gender = req.body.gender;
 
-    console.log(aiubname,username,mail,phoneno,gender);
 
-    connection.query('SELECT username FROM registration WHERE username ="' +username+'"', function (err, result) {
-        if (err) throw err;
+    // var aiubname = req.body.name.trim().toUpperCase()
+    // var username = req.body.username.trim();
+    // var mail = req.body.email.trim();
+    // var phoneno = req.body.phone.trim();
+    // var gender = req.body.gender;
 
-        console.log(result.length);
-        //checking duplicate username
-        if(result.length == 0){
-            var sql = "insert into registration values('','"+aiubname+"', '"+username+"', '"+mail+"','"+phoneno+"', '"+gender+"')";
+    // console.log(aiubname,username,mail,phoneno,gender);
+
+    // connection.query('SELECT username FROM registration WHERE username ="' +username+'"', function (err, result) {
+    //     if (err) throw err;
+
+    //     console.log(result.length);
+    //     //checking duplicate username
+    //     if(result.length == 0){
+    //         var sql = "insert into registration values('','"+aiubname+"', '"+username+"', '"+mail+"','"+phoneno+"', '"+gender+"')";
             
-            connection.query(sql, function (err) {
-                if (err){
-                    res.sendFile(__dirname+"/Front-End/fail.html");
-                    console.log(err);
-                    return;
-                }
-                else{
-                    res.sendFile(__dirname+"/Front-End/subscribed.html");
-                }
-            });
-            console.log("Username not exist, easy to enter data into database");
-        }else{
-            console.log('Username exist!!');
-            res.sendFile(__dirname+"/Front-End/fail.html");
-        }
+    //         connection.query(sql, function (err) {
+    //             if (err){
+    //                 res.sendFile(__dirname+"/Front-End/fail.html");
+    //                 console.log(err);
+    //                 return;
+    //             }
+    //             else{
+    //                 res.sendFile(__dirname+"/Front-End/subscribed.html");
+    //             }
+    //         });
+    //         console.log("Username not exist, easy to enter data into database");
+    //     }else{
+    //         console.log('Username exist!!');
+    //         res.sendFile(__dirname+"/Front-End/fail.html");
+    //     }
     
 
-      });
+    //   });
 
     
 
